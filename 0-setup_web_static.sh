41a6ecbc246d2fefbd273d307901040791e0795e
N-Steph <snouaffo@gmail.com>
2024-04-05 14:02:23 +0000

    changed egrep command to grep -E

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo bash -c 'echo "This is a test" > /data/web_static/releases/test/index.html'
 sudo chown -R ubuntu:ubuntu /data/
-RESULT=$(egrep "hbnb_static" /etc/nginx/sites-available/default)
+RESULT=$(grep -E "hbnb_static" /etc/nginx/sites-available/default)
 if [ ${#RESULT} -eq 0 ]
 then
     sudo sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n/" /etc/nginx/sites-available/default

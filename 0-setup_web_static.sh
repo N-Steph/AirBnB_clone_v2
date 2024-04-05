9557fad176201d390aa4c514f34d4b87ea928cc8
N-Steph <snouaffo@gmail.com>
2024-04-05 13:38:12 +0000

    added a conditional statement for and silent the apt-get command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 # setting up a web server for deployment
 
-sudo apt update
-sudo apt install nginx
+sudo apt-get -qq update
+sudo apt-get -qq install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo bash -c 'echo "This is a test" > /data/web_static/releases/test/index.html'
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n/" /etc/nginx/sites-available/default
+RESULT=$(egrep "hbnb_static" /etc/nginx/sites-available/default)
+if [ ${#RESULT} -eq 0 ]
+then
+    sudo sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n/" /etc/nginx/sites-available/default
+fi
 sudo service nginx restart

22af3860357444cfc2ff8a538c5ab523a7431942
N-Steph <snouaffo@gmail.com>
2024-04-08 00:02:28 +0000

    define deploy function

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""do_pack function definition"""
+
+from fabric.api import local, run, put, env, runs_once
+import glob
+from os.path import exists, getctime
+
+env.hosts = ['54.210.174.151', '18.235.234.102']
+
+
+@runs_once
+def do_pack():
+    """creates a .tgz archive file for web_static folder"""
+    task = "tar -cvzf versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static"
+    if not exists("./versions"):
+        local('mkdir versions')
+    local(task)
+    # used to retrieve files/pathnames matching a specified pattern
+    list_of_files = glob.glob('versions/*')
+    latest_file = max(list_of_files, key=getctime)
+    return latest_file
+
+
+def do_deploy(archive_path):
+    """deploy a web_static archive to remote servers"""
+    name = "$(basename /tmp/*.tgz .tgz)"
+    path = "/data/web_static/releases"
+    command_list = [
+        "mkdir -p {}/{}".format(path, name),
+        "tar -xzf /tmp/*.tgz -C {}/{}".format(path, name),
+        "mv {}/{}/web_static/* {}/{}/".format(path, name, path, name),
+        "rm -rf {}/{}/web_static".format(path, name),
+        "rm -rf /data/web_static/current",
+        "ln -s {}/{} /data/web_static/current".format(path, name),
+        "rm /tmp/*.tgz"
+    ]
+    results = []
+
+    if not exists(archive_path):
+        return False
+    put(archive_path, "/tmp/")
+    for command in command_list:
+        result = run(command)
+        results.append(result.failed)
+    if True in results:
+        return False
+    return True
+
+
+def deploy():
+    """archiving and deploying web_static folder"""
+    archive_path = do_pack()
+    success = do_deploy(archive_path)
+    return success

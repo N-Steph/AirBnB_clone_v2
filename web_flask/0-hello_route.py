088a33118a7ec4d4c368ae05a5741a6155d736e8
N-Steph <snouaffo@gmail.com>
2024-04-19 12:52:20 +0000

    binded the root url to the function hello_HBNB

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""minimal flask application"""
+
+from web_flask.__init__ import app
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Returns 'Hello HBNB' string to the browser"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
